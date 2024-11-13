a27ddf5306dcfdba15246541cc39da9ab31bb124
Cristopher Masserini <61479774+CristopherMasserini@users.noreply.github.com>
2024-11-13 11:37:37 -0500

    Revert "Model Testing"
    This reverts commit 51c8da2a92c8a675bd7c2253e236992186f2865f.

--- Model.py
+++ Model.py
@@ -4,30 +4,14 @@ import math
 class KNNPenalized:
     def __init__(self, nclassifiers):
         self.nclassifiers = nclassifiers
-        self.penalties = self.calculate_step_penalties()
-
-    def calculate_step_penalties(self):
-        penalties = []
-        try:
-            step = 1 / self.nclassifiers
-            penalties = [round(1 - (i*step), 5) for i in range(0, self.nclassifiers)]
-        except ZeroDivisionError:
-            pass
-
-        return penalties
+        self.penalties = [1-(i/self.nclassifiers) for i in range(0, self.nclassifiers)]
 
     def euclidean_distance(self, a, b):
-        try:
-            return math.dist(a, b)
-        except ValueError:
-            return None
+        return math.dist(a, b)
 
     def taxi_distance(self, a, b):
-        if len(a) != len(b):
-            return None
-        else:
-            distance = 0
-            for i in range(0, len(a)):
-                distance += abs(a[i] - b[i])
-            return distance
+        distance = 0
+        for i in range(0, len(a)):
+            distance += abs(a[i] - b[i])
+        return distance
 
