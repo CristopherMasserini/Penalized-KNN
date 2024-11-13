4566f3f17b5796d89eee37bbc299994fcad4bc2a
Cristopher Masserini <61479774+CristopherMasserini@users.noreply.github.com>
2024-11-13 11:39:58 -0500

    Model Testing

--- Model.py
+++ Model.py
@@ -4,14 +4,29 @@ import math
 class KNNPenalized:
     def __init__(self, nclassifiers):
         self.nclassifiers = nclassifiers
-        self.penalties = [1-(i/self.nclassifiers) for i in range(0, self.nclassifiers)]
+        self.penalties = self.calculate_step_penalties()
+
+    def calculate_step_penalties(self):
+        penalties = []
+        try:
+            step = 1 / self.nclassifiers
+            penalties = [round(1 - (i*step), 5) for i in range(0, self.nclassifiers)]
+        except ZeroDivisionError:
+            pass
+
+        return penalties
 
     def euclidean_distance(self, a, b):
-        return math.dist(a, b)
+        try:
+            return math.dist(a, b)
+        except ValueError:
+            return None
 
     def taxi_distance(self, a, b):
-        distance = 0
-        for i in range(0, len(a)):
-            distance += abs(a[i] - b[i])
-        return distance
-
+        if len(a) != len(b):
+            return None
+        else:
+            distance = 0
+            for i in range(0, len(a)):
+                distance += abs(a[i] - b[i])
+            return distance
